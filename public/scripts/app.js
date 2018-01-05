f773b4b016ba27b68e64682f9b71b191abe72155
Cody Thompson <c.williamthompson@gmail.com>
2018-01-05 08:34:11 +0000

    started a fresher design

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -214,4 +214,23 @@ $(() => {
   });
 
 
+  /*
+    --- Form Tabs ---
+  */
+  $('.tab a').on('click', function (e) {
+
+    e.preventDefault();
+
+    $(this).parent().addClass('active');
+    $(this).parent().siblings().removeClass('active');
+
+    target = $(this).attr('href');
+
+    $('.tab-content > div').not(target).hide();
+
+    $(target).fadeIn(600);
+
+  });
+
+
 });
